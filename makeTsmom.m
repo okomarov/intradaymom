6bf85c0b11d77b3a7181e629927cbff134dc0bf3
Oleg Komarov <o.komarov11@imperial.ac.uk>
2016-11-21 20:31:05 +0000

    added linear increasing weights

--- makeTsmom.m
+++ makeTsmom.m
@@ -27,19 +27,45 @@ ptfret.vw_pos = nansum(pos,2);
 ptfret.vw_neg = nansum(neg,2);
 ptfret.vw     = nansum(pos,2) + nansum(neg,2);
 
+% Linear increasing in the extremes
+tsmom.liw      = @(val) sign(signal) .* hpr .* getliw(signal, isign(val));
+pos            = tsmom.liw(1);
+neg            = tsmom.liw(-1);
+ptfret.liw_pos = nansum(pos,2);
+ptfret.liw_neg = nansum(neg,2);
+ptfret.liw     = nansum(pos,2) + nansum(neg,2);
+
 % EW long-only
 tsmom.ew_long  = @() hpr .* (getew(1)+getew(-1));
 ptfret.ew_long = nansum(tsmom.ew_long(),2);
-
-% VW long-only
-tsmom.vw_long  = @() hpr .* (getvw(1)+getvw(-1));
-ptfret.vw_long = nansum(tsmom.vw_long(),2);
 
 % VOLW long-only
 tsmom.volw_long  = @() hpr .* (getew(1)+getew(-1)) .* (OPT_VOL_TARGET./vol);
 ptfret.volw_long = nansum(tsmom.volw_long(),2);
 
-tsmom.isign = isign;
-tsmom.getew = getew;
-tsmom.getvw = getvw;
+% VW long-only
+tsmom.vw_long  = @() hpr .* (getvw(1)+getvw(-1));
+ptfret.vw_long = nansum(tsmom.vw_long(),2);
+
+% LIW long-only
+tsmom.liw_long  = @() hpr .* (getliw(signal, isign(1)) + getliw(signal, isign(-1)));
+ptfret.liw_long = nansum(tsmom.liw_long(),2);
+
+tsmom.isign  = isign;
+tsmom.getew  = getew;
+tsmom.getvw  = getvw;
+tsmom.getliw = @getliw;
+end
+
+function w = getliw(signal, idx)
+signal(~idx) = NaN;
+signal       = abs(signal);
+
+% rank
+[nobs,nseries] = size(signal);
+z              = NaN(nobs,nseries);
+for ii = 1:nobs
+    z(ii,:) = tiedrank(signal(ii,:));
+end
+w = nan2zero(bsxfun(@rdivide,  z, nansum(z,2)));
 end
