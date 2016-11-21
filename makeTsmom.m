63b6fbd13d9d5e98cb0c5d0b5c832564688f2b12
Oleg Komarov <o.komarov11@imperial.ac.uk>
2016-11-21 19:19:24 +0000

    add long/short legs decomposition

--- makeTsmom.m
+++ makeTsmom.m
@@ -1,30 +1,42 @@
 function [ptfret,tsmom] = makeTsmom(signal, hpr, w, vol, OPT_VOL_TARGET)
 % Equal weighted
-ptfret    = table();
-isign     = @(val) sign(signal) == val;
-getew     = @(val) isign(val) ./ nansum(isign(val), 2);
-tsmom.ew  = @() sign(signal) .* hpr .* (getew(1) + getew(-1));
-ptfret.ew = nansum(tsmom.ew(),2);
+ptfret        = table();
+isign         = @(val) sign(signal) == val;
+getew         = @(val) isign(val) ./ nansum(isign(val), 2);
+tsmom.ew      = @(val) sign(signal) .* hpr .* getew(val);
+pos           = tsmom.ew(1);
+neg           = tsmom.ew(-1);
+ptfret.ew_pos = nansum(pos,2);
+ptfret.ew_neg = nansum(neg,2);
+ptfret.ew     = nansum(pos,2) + nansum(neg,2);
 
 % Volatility weighted
-tsmom.volw  = @() sign(signal) .* hpr .* (getew(1) + getew(-1)) .* (OPT_VOL_TARGET./vol);
-ptfret.volw = nansum(tsmom.volw(),2);
+tsmom.volw      = @(val) sign(signal) .* hpr .* getew(val) .* (OPT_VOL_TARGET./vol);
+pos             = tsmom.volw(1);
+neg             = tsmom.volw(-1);
+ptfret.volw_pos = nansum(pos,2);
+ptfret.volw_neg = nansum(neg,2);
+ptfret.volw     = nansum(pos,2) + nansum(neg,2);
 
 % Value weighted
-getvw     = @(val) w .* isign(val) ./ nansum(w .* isign(val), 2);
-tsmom.vw  = @() sign(signal) .* hpr .* (getvw(1) + getvw(-1));
-ptfret.vw = nansum(tsmom.vw(),2);
+getvw         = @(val) w .* isign(val) ./ nansum(w .* isign(val), 2);
+tsmom.vw      = @(val) sign(signal) .* hpr .* getvw(val);
+pos           = tsmom.vw(1);
+neg           = tsmom.vw(-1);
+ptfret.vw_pos = nansum(pos,2);
+ptfret.vw_neg = nansum(neg,2);
+ptfret.vw     = nansum(pos,2) + nansum(neg,2);
 
 % EW long-only
-tsmom.ew_long  = @() hpr .* (getew(1) + getew(-1));
+tsmom.ew_long  = @() hpr .* (getew(1)+getew(-1));
 ptfret.ew_long = nansum(tsmom.ew_long(),2);
 
 % VW long-only
-tsmom.vw_long  = @() hpr .* (getvw(1) + getvw(-1));
+tsmom.vw_long  = @() hpr .* (getvw(1)+getvw(-1));
 ptfret.vw_long = nansum(tsmom.vw_long(),2);
 
 % VOLW long-only
-tsmom.volw_long  = @() hpr .* (getew(1) + getew(-1)) .* (OPT_VOL_TARGET./vol);
+tsmom.volw_long  = @() hpr .* (getew(1)+getew(-1)) .* (OPT_VOL_TARGET./vol);
 ptfret.volw_long = nansum(tsmom.volw_long(),2);
 
 tsmom.isign = isign;
--- utils/regressOnLongOnly.m
+++ utils/regressOnLongOnly.m
@@ -1,13 +1,17 @@
 function results = regressOnLongOnly(results, OPT_REGRESSION_LONG_MINOBS)
 opts = {'intercept',false,'display','off','type','HAC','bandwidth',floor(4*(results.N/100)^(2/9))+1,'weights','BT'};
 
-fields  = results.Names;
-fields  = fields(cellfun(@isempty, regexp(fields,'_long')));
+fields  = regexp(results.Names,'\w+(?=_long)','match','once');
+fields  = fields(~cellfun(@isempty, fields));
 nfields = numel(fields);
 for ii = 1:nfields
     f          = fields{ii};
     l          = ones(results.N, 1);
-    X          = num2cell(results.tsmom.(f)() * 100,1);
+    X          = results.tsmom.(f)(1);
+    tmp        = results.tsmom.(f)(-1);
+    idx        = results.tsmom.isign(-1);
+    X(idx)     = tmp(idx);
+    X          = num2cell(X * 100,1);
     y          = results.tsmom.([f '_long'])()*100;
     enough_obs = sum(~isnan(y)) > OPT_REGRESSION_LONG_MINOBS;
     y          = num2cell(y,1);
