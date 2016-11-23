d23dc8de66bcb01605ba799bdb70535330d28093
Oleg Komarov <o.komarov11@imperial.ac.uk>
2016-11-23 19:40:14 +0000

    add sub-period predictability

--- predictability.m
+++ predictability.m
@@ -1,16 +1,24 @@
 %% Opts
-OPT_NOMICRO = true;
+OPT_NOMICRO    = true;
+OPT_DATE_RANGE = [19900101, 20010131];
 
 OPT_RANGES = [930, 1000, 1030, 1100, 1130, 1200, 1230, 1300, 1330, 1400, 1430, 1500,1530]'*100;
 OPT_LAG    = 1;
 
-OPT_STANDARDIZE = true;
-OPT_VOL_AVG     = 's';
-OPT_LAG_VOL     = 40;
+OPT_VOL_STANDARDIZE = true;
+OPT_VOL_AVG         = 'e';
+OPT_VOL_LAG         = 40;
+OPT_VOL_SHIFT       = OPT_VOL_LAG-1+OPT_LAG;
 %% Data
 
 % Index data
 mst = loadresults('master');
+
+% SUB-PERIOD
+if ~isempty(OPT_DATE_RANGE)
+    idx = in(mst.Date, OPT_DATE_RANGE, '[]');
+    mst = mst(idx,:);
+end
 
 % Taq open price
 taq                   = loadresults('price_fl');
@@ -48,7 +56,7 @@ clear tmp
 % isequal(vol.Permno, ret.Permno)
 % isequal(vol.Date, ret.Date)
 
-% Extract 
+% Extract
 permnos = ret.Permno;
 dates   = ret.Date;
 ret     = ret{:,3:end};
@@ -56,31 +64,33 @@ vol     = vol{:,3:end};
 
 % Group observations by dates, for clustered standard errors
 [~,~,g] = unique(dates);
+
+clear tname taq
 %% Sigma
 
 % 40-day moving average lagged 1 standard deviation
-sigma = sqrt(tsmovavg(vol,OPT_VOL_AVG, OPT_LAG_VOL,1));
+sigma = sqrt(tsmovavg(vol,OPT_VOL_AVG, OPT_VOL_LAG,1));
 sigma = [NaN(OPT_LAG,size(sigma,2)); sigma(1:end-OPT_LAG,:)]; % ex-ante
 
 % Do not use lags from other permnos
-SHIFT            = OPT_LAG_VOL-1+OPT_LAG;
-igroup           = [false(SHIFT,1); permnos(1+SHIFT:end) == permnos(1:end-SHIFT)];
-sigma(~igroup,:) = NaN;
+idx           = [false(OPT_VOL_SHIFT,1); permnos(1+OPT_VOL_SHIFT:end) == permnos(1:end-OPT_VOL_SHIFT)];
+sigma(~idx,:) = NaN;
+clear idx pos
+
 %% NaN out
-inan = any(isnan(ret),2) | any(isnan(sigma),2);
+inan        = any(isnan(ret),2) | any(isnan(sigma),2);
 ret(inan,:) = NaN;
 %% Multivariate
 % Regress
 % h13 ~ constant + h11, h10 ... h1
 % h12 ~ constant + h10, h19 ... h1
 % ...
-
 n              = numel(OPT_RANGES);
 [tstat, coeff] = deal(NaN(n-1,n-2));
 for ypos = n:-1:3
     % Standardizing returns by vol
     xpos = 1:ypos-2;
-    if OPT_STANDARDIZE
+    if OPT_VOL_STANDARDIZE
         X = ret(:,xpos)./sigma(:,xpos);
         y = ret(:,ypos)./sigma(:,ypos);
     else
@@ -93,6 +103,10 @@ for ypos = n:-1:3
     tstat(r,c) = tb.tStat;
     coeff(r,c) = tb.Estimate;
 end
+results.tstat = tstat;
+results.coeff = coeff;
+clear tstat coeff X y ypos xpos tb r c
+
 %% Univariate
 % Regress
 % h13 ~ constant + h11
@@ -101,13 +115,12 @@ end
 % h12 ~ constant + h10
 % h12 ~ constant + h9
 % ...
-
 n              = numel(OPT_RANGES);
 [tstat, coeff] = deal(NaN(n-1,n-2));
 for ypos = n:-1:3
     for xpos = ypos-2:-1:1
         % Standardizing returns by vol
-        if OPT_STANDARDIZE
+        if OPT_VOL_STANDARDIZE
             X = ret(:,xpos)./sigma(:,xpos);
             y = ret(:,ypos)./sigma(:,ypos);
         else
@@ -121,3 +134,7 @@ for ypos = n:-1:3
         coeff(r,c) = tb.Estimate(2);
     end
 end
+results.tstat = tstat;
+results.coeff = coeff;
+
+clear tstat coeff X y ypos xpos tb r c
