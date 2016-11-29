3d5aa175425834bdd67d618887fa249664f5b4ba
Oleg Komarov <o.komarov11@imperial.ac.uk>
2016-11-29 16:22:53 +0000

    set base case and added overnight and skip option

--- predictability.m
+++ predictability.m
@@ -1,152 +1,166 @@
-function results = predictability(isMulti, OPT_DATE_RANGE, OPT_VOL_STANDARDIZE, OPT_VOL_TYPE, OPT_VOL_LAG)
+function [tstat,coeff,OPT_] = predictability(varargin)
 %% Opts
-OPT_NOMICRO = true;
-% OPT_DATE_RANGE = [-inf, 20010131];
-% OPT_DATE_RANGE = [20010201, inf];
-% OPT_DATE_RANGE = [];
+p              = inputParser();
+p.StructExpand = nargin == 1 && isstruct(varargin{1});
 
-OPT_RANGES = [930, 1000, 1030, 1100, 1130, 1200, 1230, 1300, 1330, 1400, 1430, 1500,1530]'*100;
-OPT_LAG    = 1;
+addOptional(p,'PREDICT_MULTI'       ,true);
+addOptional(p,'PREDICT_SKIP'        ,1);
+addOptional(p,'NO_MICRO'            ,true);
+addOptional(p,'DAY_LAG'             ,1);
+addOptional(p,'DATE_RANGE'          ,[]); % e.g. [-inf, 20010431], [20010501, inf];
+addOptional(p,'RET_USE_OVERNIGHT'   ,false);
+addOptional(p,'VOL_STANDARDIZE'     ,true);
+addOptional(p,'VOL_TYPE'            ,'e');
+addOptional(p,'VOL_LAG'             ,60);
 
-% OPT_VOL_STANDARDIZE = false;
-% OPT_VOL_TYPE        = 's';
-% OPT_VOL_LAG         = 40;
-OPT_VOL_SHIFT = OPT_VOL_LAG-1+OPT_LAG;
+p.parse(varargin{:});
+OPT_ = p.Results;
+
+OPT_.RANGES    = [930, 1000, 1030, 1100, 1130, 1200, 1230, 1300, 1330, 1400, 1430, 1500,1530]'*100;
+OPT_.VOL_SHIFT = OPT_.VOL_LAG-1+OPT_.DAY_LAG;
 %% Data
-
-% Index data
 mst = loadresults('master');
 
-% SUB-PERIOD
-if ~isempty(OPT_DATE_RANGE)
-    idx = in(mst.Date, OPT_DATE_RANGE, '[]');
-    mst = mst(idx,:);
+% Sub-period
+idx = in(mst.Date, OPT_.DATE_RANGE, '[]');
+mst = mst(idx,:);
+
+if OPT_.NO_MICRO
+    taq                  = loadresults('price_fl');
+    [idx,pos]            = ismembIdDate(mst.Permno, mst.Date,taq.Permno,taq.Date);
+%     mst.FirstPrice(idx,1) = taq.FirstPrice(pos(idx));
+    mst.LastPrice(idx,1) = taq.LastPrice(pos(idx));
+    idx                  = isMicrocap(mst, 'LastPrice',OPT_.DAY_LAG);
+    mst                  = mst(~idx,:);
+    clear taq
 end
 
-% Taq open price
-taq                   = loadresults('price_fl');
-[idx,pos]             = ismembIdDate(mst.Permno, mst.Date,taq.Permno,taq.Date);
-mst.FirstPrice(idx,1) = taq.FirstPrice(pos(idx));
-mst.LastPrice(idx,1)  = taq.LastPrice(pos(idx));
-
-if OPT_NOMICRO
-    idx = isMicrocap(mst, 'LastPrice',OPT_LAG);
-    mst = mst(~idx,:);
-end
-
-% Returns
+% Overnight
 ret = mst(:,{'Permno','Date'});
 ret = ret(ret.Permno ~= 84398,:);
-for ii = 1:numel(OPT_RANGES)
-    tmp         = loadresults(sprintf('halfHourRet%d',OPT_RANGES(ii)));
+if OPT_.RET_USE_OVERNIGHT
+    reton          = loadresults('return_intraday_overnight','..\hfandlow\results');
+    [~,pos]        = ismembIdDate(ret.Permno, ret.Date, reton.Permno, reton.Date);
+    ret.RetCO(:,1) = reton.RetCO(pos);
+    clear reton
+end
+% Returns
+for ii = 1:numel(OPT_.RANGES)
+    tmp         = loadresults(sprintf('halfHourRet%d',OPT_.RANGES(ii)));
     [~,pos]     = ismembIdDate(ret.Permno, ret.Date, tmp.Permno, tmp.Date);
-    tname       = sprintf('T%d',OPT_RANGES(ii));
+    tname       = sprintf('T%d',OPT_.RANGES(ii));
     ret.(tname) = double(tmp.(tname)(pos));
 end
-ret = sortrows(ret,{'Permno','Date'});
-
-% Variance
-vol = mst(:,{'Permno','Date'});
-vol = vol(vol.Permno ~= 84398,:);
-for ii = 1:numel(OPT_RANGES)
-    tmp         = loadresults(sprintf('halfHourVol%d',OPT_RANGES(ii)));
-    [~,pos]     = ismembIdDate(vol.Permno, vol.Date, tmp.Permno, tmp.Date);
-    tname       = sprintf('RV5_%d',OPT_RANGES(ii));
-    vol.(tname) = double(tmp.(tname)(pos));
-end
-vol = sortrows(vol,{'Permno','Date'});
-clear tmp
-% isequal(vol.Permno, ret.Permno)
-% isequal(vol.Date, ret.Date)
-
-% Extract
+ret     = sortrows(ret,{'Permno','Date'});
 permnos = ret.Permno;
 dates   = ret.Date;
 ret     = ret{:,3:end};
-vol     = vol{:,3:end};
 
-% Group observations by dates, for clustered standard errors
-[~,~,g] = unique(dates);
+% Variance
+if OPT_.VOL_STANDARDIZE
+    vol = mst(:,{'Permno','Date'});
+    vol = vol(vol.Permno ~= 84398,:);
+    if OPT_.RET_USE_OVERNIGHT
+        vol.RVCO = ret(:,1).^2;
+    end
+    for ii = 1:numel(OPT_.RANGES)
+        tmp         = loadresults(sprintf('halfHourVol%d',OPT_.RANGES(ii)));
+        [~,pos]     = ismembIdDate(vol.Permno, vol.Date, tmp.Permno, tmp.Date);
+        tname       = sprintf('RV5_%d',OPT_.RANGES(ii));
+        vol.(tname) = double(tmp.(tname)(pos));
+    end
+    vol = sortrows(vol,{'Permno','Date'});
+    % isequal(vol.Permno, ret.Permno)
+    % isequal(vol.Date, ret.Date)
+    vol = vol{:,3:end};
+    
+    % Moving average lagged 1 standard deviation
+    vol = sqrt(tsmovavg(vol,OPT_.VOL_TYPE, OPT_.VOL_LAG,1));
+    vol = [NaN(OPT_.DAY_LAG,size(vol,2)); vol(1:end-OPT_.DAY_LAG,:)]; % ex-ante
+    
+    % Do not use lags from other permnos
+    idx         = [false(OPT_.VOL_SHIFT,1); permnos(1+OPT_.VOL_SHIFT:end) == permnos(1:end-OPT_.VOL_SHIFT)];
+    vol(~idx,:) = NaN;
+else
+    vol = [];
+end
+clear idx pos tmp
 
-clear tname taq
-%% Sigma
-% 40-day moving average lagged 1 standard deviation
-sigma = sqrt(tsmovavg(vol,OPT_VOL_TYPE, OPT_VOL_LAG,1));
-sigma = [NaN(OPT_LAG,size(sigma,2)); sigma(1:end-OPT_LAG,:)]; % ex-ante
-
-% Do not use lags from other permnos
-idx           = [false(OPT_VOL_SHIFT,1); permnos(1+OPT_VOL_SHIFT:end) == permnos(1:end-OPT_VOL_SHIFT)];
-sigma(~idx,:) = NaN;
-clear idx pos
-%% NaN out
-inan        = any(isnan(ret),2) | any(isnan(sigma),2);
+% Require data on whole day
+inan = any(isnan(ret),2);
+if OPT_.VOL_STANDARDIZE
+    inan = inan | any(isnan(vol),2);
+end
 ret(inan,:) = NaN;
 
 %% Estimate
-if isMulti
-    [tstat, coeff] = estimateMulti();
+[~,~,g] = unique(dates);
+if OPT_.PREDICT_MULTI
+    [tstat, coeff] = estimateMultivariate(ret,vol,g,OPT_);
 else
-    [tstat, coeff] = estimateUni();
+    [tstat, coeff] = estimateUnivariate(ret,vol,g,OPT_);
 end
 
-str_ranges    = arrayfun(@(x) sprintf('h%d',x),OPT_RANGES/100,'un',0)';
-f             = @(x) array2table(x,'VariableNames',['c' str_ranges(1:end-2)],'RowNames',str_ranges(end:-1:3));
-results.tstat = f(tstat);
-results.coeff = f(coeff*100);
-%% Regressions
-    function [tstat, coeff] = estimateMulti()
-    % Multivariate
-    % Regress
-    % h13 ~ constant + h11, h10 ... h1
-    % h12 ~ constant + h10, h19 ... h1
-    % ...
-        n              = numel(OPT_RANGES);
-        [tstat, coeff] = deal(NaN(n-2,n-1));
-        for ypos = 3:n
-            % Standardizing returns by vol
-            xpos = 1:ypos-2;
-            if OPT_VOL_STANDARDIZE
-                X = ret(:,xpos)./sigma(:,xpos);
-                y = ret(:,ypos)./sigma(:,ypos);
-            else
-                X = ret(:,xpos);
-                y = ret(:,ypos);
-            end
-            tb         = clusterreg(y,X,g,'linear');
-            c          = 1:xpos(end)+1;
-            r          = n-ypos+1;
-            tstat(r,c) = tb.tStat;
-            coeff(r,c) = tb.Estimate;
-        end
-    end
+str_ranges = arrayfun(@(x) sprintf('h%d',x),OPT_.RANGES/100,'un',0)';
+if OPT_.RET_USE_OVERNIGHT
+    str_ranges = ['ON',str_ranges];
+end
+f     = @(x) array2table(x,'VariableNames',['c' str_ranges(1:end-OPT_.PREDICT_SKIP-1)],'RowNames',str_ranges(end:-1:2+OPT_.PREDICT_SKIP));
+tstat = f(tstat);
+coeff = f(coeff*100);
 
-    function [tstat, coeff] = estimateUni()
-    % Univariate
-    % Regress
-    % h13 ~ constant + h11
-    % h13 ~ constant + h10
-    % ...
-    % h12 ~ constant + h10
-    % h12 ~ constant + h9
-    % ...
-        n              = numel(OPT_RANGES);
-        [tstat, coeff] = deal(NaN(n-2,n-1));
-        for ypos = 3:n
-            for xpos = 1:ypos-2
-                % Standardizing returns by vol
-                if OPT_VOL_STANDARDIZE
-                    X = ret(:,xpos)./sigma(:,xpos);
-                    y = ret(:,ypos)./sigma(:,ypos);
-                else
-                    X = ret(:,xpos);
-                    y = ret(:,ypos);
-                end
-                tb         = clusterreg(y,X,g,'linear');
-                c          = xpos+1;
-                r          = n-ypos+1;
-                tstat(r,c) = tb.tStat(2);
-                coeff(r,c) = tb.Estimate(2);
-            end
-        end
+end
+
+function [tstat, coeff] = estimateMultivariate(ret,sigma,g,OPT_)
+% h13 ~ constant + h12, h11 ... h1
+% h12 ~ constant + h11, h10 ... h1
+% ...
+n              = size(ret,2);
+ncols          = n - OPT_.PREDICT_SKIP;
+nrows          = ncols - 1; % no intercept
+[tstat, coeff] = deal(NaN(nrows, ncols));
+for ypos = 2+OPT_.PREDICT_SKIP:n
+    xpos = 1:ypos-1-OPT_.PREDICT_SKIP;
+    if OPT_.VOL_STANDARDIZE
+        X = ret(:,xpos)./sigma(:,xpos);
+        y = ret(:,ypos)./sigma(:,ypos);
+    else
+        X = ret(:,xpos);
+        y = ret(:,ypos);
     end
+    tb         = clusterreg(y,X,g,'linear');
+    c          = 1:xpos(end)+1;
+    r          = nrows-ypos + 2 + OPT_.PREDICT_SKIP;
+    tstat(r,c) = tb.tStat;
+    coeff(r,c) = tb.Estimate;
+end
+end
+
+function [tstat, coeff] = estimateUnivariate(ret,sigma,g,OPT_)
+% h13 ~ constant + h12
+% h13 ~ constant + h11
+% ...
+% h12 ~ constant + h11
+% h12 ~ constant + h10
+% ...
+n              = size(ret,2);
+ncols          = n - OPT_.PREDICT_SKIP;
+nrows          = ncols - 1; % no intercept
+[tstat, coeff] = deal(NaN(nrows, ncols));
+for ypos = 2+OPT_.PREDICT_SKIP:n
+    for xpos = 1:ypos-1-OPT_.PREDICT_SKIP
+        if OPT_.VOL_STANDARDIZE
+            X = ret(:,xpos)./sigma(:,xpos);
+            y = ret(:,ypos)./sigma(:,ypos);
+        else
+            X = ret(:,xpos);
+            y = ret(:,ypos);
+        end
+        tb         = clusterreg(y,X,g,'linear');
+        c          = xpos+1;
+        r          = nrows-ypos + 2 + OPT_.PREDICT_SKIP;
+        tstat(r,c) = tb.tStat(2);
+        coeff(r,c) = tb.Estimate(2);
+    end
+end
 end
