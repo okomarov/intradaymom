886d9454f049542df5b5f0f7260115c50a0af9e3
Oleg Komarov <o.komarov11@imperial.ac.uk>
2016-12-11 17:31:10 +0000

    bear and bull market predictability

--- main_predictability.m
+++ main_predictability.m
@@ -1,28 +1,36 @@
-t = {}; c = {}; o = {};
-% DEFAULT
-[t{end+1},c{end+1},o{end+1}] = predictability('PREDICT_MULTI',1,'PREDICT_SKIP',1,'VOL_STANDARDIZE',1,'VOL_TYPE','e','VOL_LAG',60);
-[t{end+1},c{end+1},o{end+1}] = predictability('PREDICT_MULTI',0,'PREDICT_SKIP',1,'VOL_STANDARDIZE',1,'VOL_TYPE','e','VOL_LAG',60);
-% SIMPLE VOL
-[t{end+1},c{end+1},o{end+1}] = predictability('PREDICT_MULTI',1,'VOL_TYPE','s');
-% [t{end+1},c{end+1},o{end+1}] = predictability('PREDICT_MULTI',0,'VOL_TYPE','s');
-% NO VOL
-[t{end+1},c{end+1},o{end+1}] = predictability('PREDICT_MULTI',1,'VOL_STANDARDIZE',0);
-% [t{end+1},c{end+1},o{end+1}] = predictability('PREDICT_MULTI',0,'VOL_STANDARDIZE',0);
-% NO SKIP
-[t{end+1},c{end+1},o{end+1}] = predictability('PREDICT_MULTI',1,'PREDICT_SKIP',0);
-% [t{end+1},c{end+1},o{end+1}] = predictability('PREDICT_MULTI',0,'PREDICT_SKIP',0);
-% OVERNIGHT
-[t{end+1},c{end+1},o{end+1}] = predictability('PREDICT_MULTI',1,'RET_USE_OVERNIGHT',1);
-% [t{end+1},c{end+1},o{end+1}] = predictability('PREDICT_MULTI',0,'RET_USE_OVERNIGHT',1);
-% 10 LOOKBACK
-[t{end+1},c{end+1},o{end+1}] = predictability('PREDICT_MULTI',1,'VOL_LAG',10);
-% [t{end+1},c{end+1},o{end+1}] = predictability('PREDICT_MULTI',0,'VOL_LAG',10);
-% 1993-2001
-[t{end+1},c{end+1},o{end+1}] = predictability('PREDICT_MULTI',1,'DATE_RANGE',[-inf, 20010431]);
-% [t{end+1},c{end+1},o{end+1}] = predictability('PREDICT_MULTI',0,'DATE_RANGE',[-inf, 20010431]);
-% 2001-2010
-[t{end+1},c{end+1},o{end+1}] = predictability('PREDICT_MULTI',1,'DATE_RANGE',[20010501,inf]);
-% [t{end+1},c{end+1},o{end+1}] = predictability('PREDICT_MULTI',0,'DATE_RANGE',[20010501,inf]);
-% Skip 2007-2009 recession
-[t{end+1},c{end+1},o{end+1}] = predictability('PREDICT_MULTI',1,'DATE_RANGE',{[-inf,20071214] [20090616, inf]});
-% [t{end+1},c{end+1},o{end+1}] = predictability('PREDICT_MULTI',0,'DATE_RANGE',{[-inf,20071214] [20090616, inf]});
+t = {}; c = {}; R = {}; o = {};
+names = {'default'};
+[t{end+1},c{end+1},R{end+1},o{end+1}] = predictability('PREDICT_MULTI',1,'PREDICT_SKIP',1,'VOL_STANDARDIZE',1,'VOL_TYPE','e','VOL_LAG',60);
+names = [names; 'univariate'];
+[t{end+1},c{end+1},R{end+1},o{end+1}] = predictability('PREDICT_MULTI',0,'PREDICT_SKIP',1,'VOL_STANDARDIZE',1,'VOL_TYPE','e','VOL_LAG',60);
+names = [names; 'simple vol'];
+[t{end+1},c{end+1},R{end+1},o{end+1}] = predictability('PREDICT_MULTI',1,'VOL_TYPE','s');
+% [t{end+1},c{end+1},R{end+1},o{end+1}] = predictability('PREDICT_MULTI',0,'VOL_TYPE','s');
+names = [names; 'no vol'];
+[t{end+1},c{end+1},R{end+1},o{end+1}] = predictability('PREDICT_MULTI',1,'VOL_STANDARDIZE',0);
+% [t{end+1},c{end+1},R{end+1},o{end+1}] = predictability('PREDICT_MULTI',0,'VOL_STANDARDIZE',0);
+names = [names; 'no skip'];
+[t{end+1},c{end+1},R{end+1},o{end+1}] = predictability('PREDICT_MULTI',1,'PREDICT_SKIP',0);
+% [t{end+1},c{end+1},R{end+1},o{end+1}] = predictability('PREDICT_MULTI',0,'PREDICT_SKIP',0);
+names = [names; 'with overnight'];
+[t{end+1},c{end+1},R{end+1},o{end+1}] = predictability('PREDICT_MULTI',1,'RET_USE_OVERNIGHT',1);
+% [t{end+1},c{end+1},R{end+1},o{end+1}] = predictability('PREDICT_MULTI',0,'RET_USE_OVERNIGHT',1);
+names = [names; '10day lookback'];
+[t{end+1},c{end+1},R{end+1},o{end+1}] = predictability('PREDICT_MULTI',1,'VOL_LAG',10);
+% [t{end+1},c{end+1},R{end+1},o{end+1}] = predictability('PREDICT_MULTI',0,'VOL_LAG',10);
+names = [names; 'pre-decimalization'];
+[t{end+1},c{end+1},R{end+1},o{end+1}] = predictability('PREDICT_MULTI',1,'DATE_RANGE',[-inf, 20010431]);
+% [t{end+1},c{end+1},R{end+1},o{end+1}] = predictability('PREDICT_MULTI',0,'DATE_RANGE',[-inf, 20010431]);
+names = [names; 'post-decimalization'];
+[t{end+1},c{end+1},R{end+1},o{end+1}] = predictability('PREDICT_MULTI',1,'DATE_RANGE',[20010501,inf]);
+% [t{end+1},c{end+1},R{end+1},o{end+1}] = predictability('PREDICT_MULTI',0,'DATE_RANGE',[20010501,inf]);
+names = [names; 'skip 2007-2009 recession'];
+[t{end+1},c{end+1},R{end+1},o{end+1}] = predictability('PREDICT_MULTI',1,'DATE_RANGE',{[-inf,20071214] [20090616, inf]});
+% [t{end+1},c{end+1},R{end+1},o{end+1}] = predictability('PREDICT_MULTI',0,'DATE_RANGE',{[-inf,20071214] [20090616, inf]});
+names = [names; 'bull markets'];
+[t{end+1},c{end+1},R{end+1},o{end+1}] = predictability('PREDICT_MULTI',1,'DATE_RANGE',{[-inf,20000101] [20020601, 20070701] [20090101 inf]});
+names = [names; 'bear markets'];
+[t{end+1},c{end+1},R{end+1},o{end+1}] = predictability('PREDICT_MULTI',1,'DATE_RANGE',{[20000101 20020601], [20070701 20090101]});
+
+out = array2table([t; c; R; o]','VariableNames',{'tstat','coeff','R','opts'},'RowNames',names);
+save predict_results out
