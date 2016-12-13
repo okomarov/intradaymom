0f3c81b816c8f361ba6c09b36cac71cae340e117
Oleg Komarov <o.komarov11@imperial.ac.uk>
2016-12-13 19:45:15 +0000

    use new TSMOM, and deprecate bivariate estimateTSmom

--- estimateTsmom.m
+++ estimateTsmom.m
@@ -1,38 +1,24 @@
-function [ptfret, stats] = estimateTsmom(results, opt, fun, names, varargin)
+function [ptfret, stats, signal, hpr, lvl, dt, h] = estimateTSmom(spec_sig, spec_hpr, mst, price_fl, reton, OPT_, doplot, dates)
+signal = getIntradayRet(spec_sig, mst, price_fl, OPT_.DATAPATH);
+hpr    = getIntradayRet(spec_hpr, mst, price_fl, OPT_.DATAPATH)*100;
 
-if ~isempty(opt.DATE_RANGE)
-    idx = in(results.dates,opt.DATE_RANGE,'[]');
-else
-    idx = true(numel(results.dates),1);
+if OPT_.RET_USE_OVERNIGHT
+    signal = (1+signal) .* (1 + reton) - 1;
 end
 
-ptfret.univariate = makeTsmom(results.signal, results.hpr,fun);
-stats.univariate  = stratstats(results.dates(idx,:), ptfret.univariate(idx,:),'Frequency','d','IsPercentageReturn',true)';
+[ptfret, avg_sig] = makeTsmom(signal, hpr, @(w,l) w-l);
 
-if isempty(opt.NUM_PTF) || opt.NUM_PTF < 2
-    return
-end
+stats = fstat(dates, ptfret, avg_sig)';
 
-for ii = 1:numel(names)
-    f = names{ii};
-    if strcmpi(f, 'industry')
-        ptfret.(f) = makeTsmomBiv(results, varargin{ii}, fun, struct('PortfolioNumber', numel(unique(varargin{ii}))));
-    else
-        ptfret.(f) = makeTsmomBiv(results, varargin{ii}, fun, struct('PortfolioNumber',opt.NUM_PTF));
-    end
-    stats.(f) = stratstats(results.dates(idx,:), ptfret.(f)(idx,:),'d',0)';
+if doplot
+    [lvl,dt,h] = plot_cumret(dates, ptfret{:,:}./100, 1, true);
+    legend win lose wml long
+    legend location northwest
 end
 end
 
-function out = makeTsmomBiv(results, signal, fun, opts)
-out = table();
-
-[bins, countd, ptf_id] = binSignal(signal,opts);
-for p = 1:max(ptf_id)
-    idx          = bins == p;
-    signal       = results.signal;
-    signal(~idx) = NaN;
-    tmp          = makeTsmom(signal, results.hpr, fun);
-    out          = [out renameVarNames(tmp, strcat(getVariableNames(tmp),sprintf('_%d',p)))];
-end
+function stats = fstat(dt,ptfret,signal)
+signal = [nanmean(signal,1), NaN(1,size(ptfret,2) - size(signal,2))];
+stats  = stratstats(dt, ptfret,'Frequency','d','IsPercentageReturn',true);
+stats  = [stats,  table(signal', 'VariableNames',{'Avgsig'})];
 end
