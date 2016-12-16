7f625d7dbdc031c9b6bda0ee55fa6f2b72f12727
Oleg Komarov <o.komarov11@imperial.ac.uk>
2016-12-16 19:43:55 +0000

    allow group standardization

--- utils/getSparkline.m
+++ utils/getSparkline.m
@@ -1,17 +1,25 @@
-function sparklines = getSparkline(v,ylim,xlim)
+function sparklines = getSparkline(v,ylim,xlim,group)
 if nargin < 2
     ylim = [0,1];
 end
-if nargin < 3
+if nargin < 3 || isempty(xlim)
     xlim = [0,1];
+end
+if nargin < 4
+    group = false;
 end
 if isvector(v)
     v = v(:)';
 end
 
 % Rescale to [0,1] and then into ylim
-vmin    = min(v,[],2);
-vmax    = max(v,[],2);
+if group
+    vmin = min(v(:));
+    vmax = max(v(:));
+else
+    vmin = min(v,[],2);
+    vmax = max(v,[],2);
+end
 vscaled = (v - vmin)./(vmax-vmin);
 vscaled = vscaled.*(ylim(2)-ylim(1)) + ylim(1);
 
