3a40668f9e089cc9ee75c3b07f84cfa100f3e677
Oleg Komarov <o.komarov11@imperial.ac.uk>
2017-01-02 15:40:00 +0000

    compare with signum version and weighted win lose

--- makeTsmom.m
+++ makeTsmom.m
@@ -4,12 +4,19 @@ ptfret = table();
 % Equal weighted
 hpr_win       = apply_mask(signal, 1,hpr);
 hpr_los       = apply_mask(signal,-1,hpr);
+count_win     = sum(~isnan(hpr_win),2);
+count_los     = sum(~isnan(hpr_los),2);
+count         = count_win + count_los;
 ptfret.ew_win = nanmean(hpr_win,2);
+ptfret.ew_win_ = ptfret.ew_win.*count_win./count;
 ptfret.ew_los = nanmean(hpr_los,2);
+ptfret.ew_los_ = ptfret.ew_los.*count_los./count;
 ptfret.ew_fun = ptfret.ew_win - ptfret.ew_los;
+ptfret.ew_sgn = (nansum(hpr_win,2) - nansum(hpr_los,2)) ./ count;
 
 % EW long-only
-ptfret.ew_long = ptfret.ew_win + ptfret.ew_los;
+ptfret.ew_long  = ptfret.ew_win + ptfret.ew_los;
+ptfret.ew_long_ = (nansum(hpr_win,2) + nansum(hpr_los,2)) ./ count;
 
 if nargout == 2
     avgsignal = [nanmean(apply_mask(signal, 1,signal),2),...
